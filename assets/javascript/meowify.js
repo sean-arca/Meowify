eeb72238b258b68b4ec96f943cc08847fe8c14a5
Sean Arca <Sean@dhcp-10-105-109-74.wireless.northwestern.private>
2018-01-08 12:47:58 -0600

    add catWords array

--- assets/javascript/meowify.js
+++ assets/javascript/meowify.js
@@ -1,4 +1,6 @@
+$(document).ready(function() {
 // List Global Variables (is it only the array?)
+var catWords = ["funny cat", "taco cat", "nyan cat", "cute cat", "party cat", "grumpy cat", "fat cat", "sad cat", "angry cat", "business cat"];
 
 // Display Buttons for GIFs
 
@@ -7,3 +9,5 @@
 // Display GIFs (thru GIPHY API)
 
 // On click pause/unpause
+
+};
